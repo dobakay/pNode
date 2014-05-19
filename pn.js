417a3ca7558592970552702d142334d09f78cda5
Putzmitt3l <dkralev90@gmail.com>
2014-05-19 20:29:20 +0300

    second commit without modules

--- pn.js
+++ pn.js
@@ -4,45 +4,48 @@ var pn = (function (config) {
 	var sender = new gcm.Sender(config.gcmApiKey),
 		deviceList = {},
 		messageList = {},
+		lastMessageId = 0,
 		pnModule = {};
 
 	function addDevice (deviceObj) {
-		//TODO check if such device exists already
 		if(deviceList[deviceObj.uuid] === undefined) {
-			deviceObject.timeStamp = new Date();
-			deviceList[device.uuid] = deviceObj;
+			deviceObj.timeStamp = new Date();
+			deviceList[deviceObj.uuid] = deviceObj;
 		}
+		console.log('Added device:' + JSON.stringify(deviceList[deviceObj.uuid]));
 	}
 
-	function removeDevice (deviecObj) {
-		if(deviceList[deviceObj.uuid] === undefined) {
-			delete deviceList[deviceObj.uuid];
+	function removeDevice (deviecObjUuid) {
+		if(deviceList[deviecObjUuid] !== undefined) {
+			console.log('Removing device:' + JSON.stringify(deviceList[deviecObjUuid]));
+			delete deviceList[deviecObjUuid];
 		}
-	}
-
-	function createMessage (messageText) {
-		var message = {
-			// id : idGenerator(),
-			text : messageText,
-			timestampString : getTime(),
-			dayStamp : new Date().getDay()
-		};
 	}
 
 	function sendMessage (message, devicesToNotify) {
+		if(devicesToNotify === undefined) {
+			//broadcasting to all devices if no-particular are specified
+			var devicesToNotify = [];
+			for (var uuid in deviceList) {
+				devicesToNotify.push(deviceList[uuid].token);
+			}
+		}
+
 		var message = new gcm.Message({
-				collapseKey: null,
+				collapseKey: 'push demo',
 				delayWhileIdle: true,
 				timeToLive: 3,
 				data: {
-					message : message//,
-					// command: command
+					message : message
 				}
 			});
-			sender.send(message, devicesToNotify, 4, function (err, result) {
-				console.log(err);
-				console.log(result);
-			});
+
+		sender.send(message, devicesToNotify, 4, function (err, result) {
+			console.log(err);
+			console.log(result);
+		});
+
+		messageList[lastMessageId++] = message;
 	}
 
 	pnModule.addDevice = addDevice;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var fs = require('fs'),
 	express = require('express'),
-	bodyparser = require('body-parser'),
+	bodyparser = require('body-parser'), //middleware specific for express (express no longer contains in it's self such mechanisms)
 	config = JSON.parse(fs.readFileSync('config.json')),
 	pn = require('./pn.js'),
 	host = config.host,
 	port = config.port,
-	//TODO: add pn module
 	app = express(),
 	pn = pn(config);
 
@@ -15,11 +14,29 @@ app.get('/', function(req, res) {
 	res.sendfile('./index.html');
 });
 
+//NOTE: needed to parse json (express using another module 'connect')
+app.use(bodyparser.json());
+
 app.post('/register', function(req, res) {
-	res.send('success');
-	res.end();
+	//NOTE: req.body is an already parsed JSON object
+	pn.addDevice(req.body); // adding device info to our simulated database
+	res.send(200);
 });
 
+app.post('/unregister', function(req, res) {
+	console.log(req.body.uuid);
+	pn.removeDevice(req.body.uuid); // removing device info from our simulated database
+	res.send(200);
+});
+
+
+//this request is sent from within the server when we want to send a message to our users
+app.post('/sendmessage', function(req, res) {
+	pn.sendMessage(req.body.message);
+	res.send(200);
+});
+
+//for cross domain calls
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -29,12 +46,7 @@ app.all('*', function(req, res, next) {
 //uploading static css/js files and whatnot
 app.use('/static', express.static(__dirname + '/static')); 
 
-//config for json consumption on post
-app.use(bodyparser.json());
-
-
-
 
 var server = app.listen(port, function() {
-	console.log('Listening on port %d', server.address().port);
+	console.log('Live & prosper on port %d', server.address().port);
 });
